0f478c72766ef7472be8b3378e6f67e88a2df170
adarsh-gowda <adarshgowda2711@gmail.com>
2024-10-27 19:07:55 +0530

    data_validation pipeline updated

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-
+from cnnClassifier.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 
 
@@ -14,3 +14,14 @@ try:
 except Exception as e:
         logger.exception(e)                                        #from cnnClassifier import logger
         raise e
+
+
+STAGE_NAME = "Data Validation stage"
+
+try:
+    config = ConfigurationManager()
+    data_validation_config = config.get_data_validation_config()
+    data_validation = DataValiadtion(config=data_validation_config)
+    data_validation.validate_all_columns()
+except Exception as e:
+    raise e
